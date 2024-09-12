6c3b93603484f9dbf1cda4780c98b3a7b6313c16
Matei-Stefan Militaru <mmilitar@car14s2.42barcelona.com>
2024-09-12 04:05:28 +0200

    C01

---
+++ C/C01/README.md
@@ -0,0 +1,11 @@
+| Number | Function    | Status  |
+|--------|-------------|---------|
+| 00     | [ft_ft.c](ex00/ft_ft.c)  | ✅       |
+| 01     | [ft_ultimate_ft.c](ex01/ft_ultimate_ft.c)  | ✅       |
+| 02     | [ft_swap.c](ex02/ft_swap.c)  | ✅       |
+| 03     | [ft_div_mod.c](ex03/ft_div_mod.c)  | ✅       |
+| 04     | [ft_ultimate_div_mod.c](ex04/ft_ultimate_div_mod.c)  | ✅       |
+| 05     | [ft_putstr.c](ex05/ft_putstr.c)   | ✅       |
+| 06     | [ft_strlen.c](ex06/ft_strlen.c)   | ✅       |
+| 07     | [ft_rev_int_tab.c](ex07/ft_rev_int_tab.c)    | ✅       |
+| 08     | [ft_sort_int_tab.c](ft_sort_int_tab.c)   | ✅       |
---
+++ C/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmilitar <mmilitar@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/11 19:22:34 by mmilitar          #+#    #+#             */
+/*   Updated: 2024/08/11 19:28:29 by mmilitar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
